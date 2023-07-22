2cdc1d1f481d1617f751a802d42f2383d0ae28b8
Reese (Yaeji) <notyaeji@gmail.com>
2023-07-22 16:59:55 +0900

    Chore: edit site config

--- site.config.js
+++ site.config.js
@@ -52,7 +52,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "reeseo3o/reese-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
